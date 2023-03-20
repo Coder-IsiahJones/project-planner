de4741af2f93fbf5177ab45d1da1f24aeec3d0e4
coder-isiahjones <jones.isiah.blhs@gmail.com>
2023-03-19 21:30:10 -0400

    set local json db

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,46 @@
 <template>
-  <div class="home">Home</div>
+  <div class="home">
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <SingleProject
+          :project="project"
+          @delete="handleDelete"
+          @complete="handleComplete"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import SingleProject from "../components/SingleProject.vue";
+
 export default {
   name: "HomeView",
-  components: {},
+  components: { SingleProject },
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/projects")
+      .then((response) => response.json())
+      .then((data) => (this.projects = data))
+      .catch((error) => console.log(error));
+  },
+  methods: {
+    handleDelete(id) {
+      this.projects = this.projects.filter((project) => {
+        return project.id !== id;
+      });
+    },
+    handleComplete(id) {
+      let projectIndex = this.projects.find((project) => {
+        return project.id === id;
+      });
+      projectIndex.complete = !projectIndex.complete;
+    },
+  },
 };
 </script>
