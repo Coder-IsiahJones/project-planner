d290a9d4c7a1b3b706bd4d4347c8128a0580ed80
coder-isiahjones <jones.isiah.blhs@gmail.com>
2023-03-25 15:28:30 -0400

    filter nav

---
+++ src/components/FilterNav.vue
@@ -0,0 +1,54 @@
+<template>
+  <nav class="filter-nav">
+    <button
+      :class="{ active: currentFilter === 'all' }"
+      @click="updateFilter('all')"
+    >
+      View All
+    </button>
+    <button
+      :class="{ active: currentFilter === 'completed' }"
+      @click="updateFilter('completed')"
+    >
+      Completed
+    </button>
+    <button
+      :class="{ active: currentFilter === 'ongoing' }"
+      @click="updateFilter('ongoing')"
+    >
+      Ongoing
+    </button>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: ["currentFilter"],
+  methods: {
+    updateFilter(filter) {
+      this.$emit("updateFilter", filter);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.filter-nav {
+  button {
+    background: none;
+    border: none;
+    color: #bbb;
+    outline: none;
+    font-size: 0.75rem;
+    text-transform: uppercase;
+    margin-right: 0.625rem;
+    letter-spacing: 0.0625rem;
+    font-weight: bold;
+    cursor: pointer;
+  }
+
+  .active {
+    color: #555;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="home">
+    <FilterNav
+      @updateFilter="currentFilter = $event"
+      :currentFilter="currentFilter"
+    />
     <div v-if="projects.length">
-      <div v-for="project in projects" :key="project.id">
+      <div v-for="project in filteredProjects" :key="project.id">
         <SingleProject
           :project="project"
           @delete="handleDelete"
@@ -14,13 +18,15 @@
 
 <script>
 import SingleProject from "../components/SingleProject.vue";
+import FilterNav from "../components/FilterNav.vue";
 
 export default {
   name: "HomeView",
-  components: { SingleProject },
+  components: { SingleProject, FilterNav },
   data() {
     return {
       projects: [],
+      currentFilter: "all",
     };
   },
   mounted() {
@@ -40,6 +46,16 @@ export default {
         return project.id === id;
       });
       projectIndex.complete = !projectIndex.complete;
+    },
+  },
+  computed: {
+    filteredProjects() {
+      if (this.currentFilter === "completed") {
+        return this.projects.filter((project) => project.complete);
+      } else if (this.currentFilter === "ongoing") {
+        return this.projects.filter((project) => !project.complete);
+      }
+      return this.projects;
     },
   },
 };
